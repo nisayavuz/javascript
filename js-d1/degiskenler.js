e7184daa242cd59f4350b17d1cdc71024372dfaa
Nisa Yavuz <nsnryvz@gmail.com>
2023-05-15 14:10:48 +0300

    first commit

---
+++ js-d1/degiskenler.js
@@ -0,0 +1,6 @@
+var name = "nisa"
+console.log(name);
+
+/******     let    ******/
+let serverName = "https://kodluyoruz.org";
+console.log(serverName);
---
+++ js-d1/helloworld.js
@@ -0,0 +1,9 @@
+//dökümana yazdırmak için:
+document.write("merhaba dunya");
+
+//Alert uyarı mesajı ile yazdırmak:
+alert("Merhaba dunya");
+
+//Konsola yazdırmak:
+console.log("merhaba dunya");
+
