22fda13d934f7ab1bdfa566e48b9b8ba34108db3
Nisa Yavuz <nsnryvz@outlook.com>
2023-06-02 21:59:13 +0300

    day 3

---
+++ js-d3/d3.js
@@ -0,0 +1,7 @@
+console.log("message from d3.js")
+
+let name = "nisa" , 
+    age = 23,
+    country = "Türkiye";
+
+console.log(name, age , country)
---
+++ js-d3/exp.js
@@ -0,0 +1,1 @@
+console.log("another js in the same html file")
