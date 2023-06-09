cf417477c611860c587c01f4264fd7a20eed1fde
Nisa Yavuz <nsnryvz@outlook.com>
2023-06-09 23:57:32 +0300

    windows methods

--- js-d5/d5.js
+++ js-d5/d5.js
@@ -1,7 +1,7 @@
 //comparison operators
-x == y // değerin aynı olup olmadığına bakar
-x === y // hem değerin hem de veri türünün eşit olup olmadığına bakar
-x != y // not equal
+//x == y değerin aynı olup olmadığına bakar
+//x === y hem değerin hem de veri türünün eşit olup olmadığına bakar
+// x != y  not equal
 
 console.log("mango".length == "avocado".length) // false
 console.log("mango".length < "avocado".length) // true
@@ -14,3 +14,13 @@ let check2 = 4>3 || 5<2 // true. (or operator.)
 
 let isLightOn = true
 let isLightOff = !isLightOn // false (negation)
+
+//ternary operator 
+let result = 5>4 ? "evet" : "hayir"
+console.log(result);
+
+/****** Window Methods *******/
+alert("welcome to my page")
+// Do not use too much alert because it is destructing and annoying, use it just to test.
+
+prompt("bir değer giriniz: ") // kullanıcıdan veri almak için kullanılır.
